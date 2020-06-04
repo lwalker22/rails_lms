485c1f12a5b17eadb4878d3d8aab51921b104e1e
Logan Walker <loganwalker22@gmail.com>
2020-06-03 20:52:09 -0600

    end of lecture

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+## Write Ruby Code in here ## ## Data to play around with##
+
+## Faker Gem - fake data to play around with
+## https://github.com/faker-ruby/faker
+# Run Seeds file : bundle exec rake db:seed
+
+roles = ['teacher', 'ta', 'student']
+10.times do
+  course = Course.create(
+    name: Faker::Science.scientist
+  )
+
+  5.times do
+    user = User.create(
+      first_name: Faker::TvShows::RickAndMorty.character,
+      last_name: Faker::Games::Zelda.character
+    )
+
+    Enrollment.create(
+      role: roles.sample,
+      course_id: course.id,
+      user_id: user.id
+    )
+  end
+end
+puts 'data seeded'
+
